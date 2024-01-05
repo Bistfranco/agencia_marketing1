c0640d038b77b6335a2f480e285121127a6ee480
Bistfranco <Bistmanfrancoagustin@gmail.com>
2024-01-04 22:15:28 -0300

    4/1

--- tailwind_components/BlogSection.jsx
+++ tailwind_components/BlogSection.jsx
@@ -65,7 +65,7 @@ const posts = [
           <div className="text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
             <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
+              Lorem ipsum dolor sit amet consecteturr, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
             </p>
           </div>
           <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
